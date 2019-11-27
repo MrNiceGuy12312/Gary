650c331b466c4a536b5d544e4bf48716377f8dca
100Vikings <jeff.gauvin@trios.com>
2019-11-27 15:37:52 -0500

    Initial commit of Mike's project.

---
+++ Adventures Of Gary/Assets/Scripts/AIBehaviours/Vision.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vision : MonoBehaviour
+{
+    private AIBrain brain;
+
+    private void Awake()
+    {
+        brain = GetComponentInParent<AIBrain>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            brain.SpotEnemy(other.transform);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            brain.LostEnemy();
+        }
+    }
+}
---
+++ Adventures Of Gary/Assets/Scripts/Character.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public int maxHitPoints = 100;
+    private int curHitPoints;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        curHitPoints = maxHitPoints;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (curHitPoints <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
+
+    public void ApplyDamage(int amount)
+    {
+        curHitPoints -= amount;
+    }
+}
