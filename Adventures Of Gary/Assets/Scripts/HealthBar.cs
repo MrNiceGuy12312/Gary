ccb40af09d91b84659be0018459cc41d765b1720
MrNiceGuy12312 <52978365+MrNiceGuy12312@users.noreply.github.com>
2019-11-28 15:55:26 -0500

    healthbar

---
+++ Adventures Of Gary/Assets/Scripts/HealthBar.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Image healthBarFill;
+    public float cautionAmount = 0.65f;
+    public float dangerAmount = 0.25f;
+
+    private void Awake()
+    {
+        enabled = false;
+    }
+    public void UpdatePercentage(float percent)
+    {
+        float percentage = Mathf.Clamp(percent, 0.0f, 1.0f);
+
+        healthBarFill.fillAmount = percentage;
+        if(percentage <= dangerAmount)
+        {
+            healthBarFill.color = Color.red;
+        }
+        else if (percentage <= cautionAmount)
+        {
+            healthBarFill.color =Color.yellow;
+        }
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        
+    }
+}
