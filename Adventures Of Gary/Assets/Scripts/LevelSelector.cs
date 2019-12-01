0bea1a072f2e070c96362dfe7b2a849db2f7118f
alexh16 <50339946+alexh16@users.noreply.github.com>
2019-12-01 01:54:29 -0500

    a

---
+++ Adventures Of Gary/Assets/Scripts/LevelSelector.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelector : MonoBehaviour
+{ 
+
+	
+
+    public void MainMenu ()
+    {
+        SceneManager.LoadScene( 0);
+
+    }
+
+    public void levelOne ()
+    {
+        SceneManager.LoadScene( 2);
+    }
+
+    public void levelTwo ()
+    {
+        SceneManager.LoadScene( 3);
+    }
+
+    public void levelThree ()
+    {
+        SceneManager.LoadScene( 4);
+    }
+
+    public void levelFour ()
+    {
+        SceneManager.LoadScene( 5);
+    }
+
+    public void levelFive ()
+    {
+        SceneManager.LoadScene( 6);
+    }
+
+    public void Intro ()
+    {
+        SceneManager.LoadScene( 7);
+    }
+    
+    public void Credits ()
+    {
+        SceneManager.LoadScene( 8);
+    }
+
+
+    public void QuitGame ()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+
+    
+}
---
+++ Adventures Of Gary/Assets/Scripts/Menu.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{ 
+
+    public void MainMenu ()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void Play ()
+	{
+        SceneManager.LoadScene(1); 
+	}
+    public void HowToPlay ()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void Settings ()
+    {
+        SceneManager.LoadScene(3);
+    }
+    public void Credits ()
+    {
+        SceneManager.LoadScene(4);
+    }
+	 public void GameOver ()
+    {
+        SceneManager.LoadScene(5);
+    }
+
+
+
+    public void QuitGame ()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+	
+    
+}
